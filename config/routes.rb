2577cb706218e67c8be2998ae0064ede76a68c85
vperez10 <vperez@chicagobooth.edu>
2015-07-21 13:48:00 -0400

    fixed Sagittarius

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   get("/zodiacs/virgo",      { :controller => "zodiacs", :action => "maiden" })
   get("/zodiacs/libra",       { :controller => "zodiacs", :action => "scales" })
   get("/zodiacs/scorpio",      { :controller => "zodiacs", :action => "scorpion" })
-  # get("/zodiacs/sagittarius", { :controller => "zodiacs", :action => "archer" })
+  get("/zodiacs/sagittarius", { :controller => "zodiacs", :action => "archer" })
   # get("/zodiacs/capricorn",   { :controller => "zodiacs", :action => "goat" })
   # get("/zodiacs/aquarius",    { :controller => "zodiacs", :action => "waterbearer" })
   # get("/zodiacs/pisces",      { :controller => "zodiacs", :action => "fish" })
